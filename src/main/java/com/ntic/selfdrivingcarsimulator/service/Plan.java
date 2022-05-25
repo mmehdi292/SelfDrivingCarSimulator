7abcfd763b0dc2e0596c5798feae6f318684b206
mmehdi292 <mmehdi292@gmail.com>
2022-05-25 15:26:45 +0100

    fixing agents problemes if the direction is same

--- src/main/java/com/ntic/selfdrivingcarsimulator/service/Plan.java
+++ src/main/java/com/ntic/selfdrivingcarsimulator/service/Plan.java
@@ -401,19 +401,59 @@ public class Plan {
         if(isInCollision){
             switch (agent.getDirection()){
                 case "TOP":
+                    if(isAgent){
+                        if(agent.getContext().agentNotSelected.getDirection().equals("TOP")){
+                            break;
+                        }
+                    }
+                    else{
+                        if(agent.getContext().agent.getDirection().equals("TOP")){
+                            break;
+                        }
+                    }
                     agent.getPhysical().setLayoutX(agent.getPhysical().getLayoutX()+10);
                     agent.setStartMoveValue(false);
 
                 break;
                 case "BOTTOM":
+                    if(isAgent){
+                        if(agent.getContext().agentNotSelected.getDirection().equals("BOTTOM")){
+                            break;
+                        }
+                    }
+                    else{
+                        if(agent.getContext().agent.getDirection().equals("BOTTOM")){
+                            break;
+                        }
+                    }
                     agent.getPhysical().setLayoutX(agent.getPhysical().getLayoutX()-10);
                     agent.setStartMoveValue(false);
                     break;
                 case "LEFT":
+                    if(isAgent){
+                        if(agent.getContext().agentNotSelected.getDirection().equals("LEFT")){
+                            break;
+                        }
+                    }
+                    else{
+                        if(agent.getContext().agent.getDirection().equals("LEFT")){
+                            break;
+                        }
+                    }
                         agent.getPhysical().setLayoutY(agent.getPhysical().getLayoutY()+10);
                     agent.setStartMoveValue(true);
                     break;
                 case "RIGHT":
+                    if(isAgent){
+                        if(agent.getContext().agentNotSelected.getDirection().equals("RIGHT")){
+                            break;
+                        }
+                    }
+                    else{
+                        if(agent.getContext().agent.getDirection().equals("RIGHT")){
+                            break;
+                        }
+                    }
                     agent.getPhysical().setLayoutY(agent.getPhysical().getLayoutY()-10);
                     agent.setStartMoveValue(true);
                     break;
